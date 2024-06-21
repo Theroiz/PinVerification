a787f6a5a50c4939580d861bfe7bc5eb4e28b8a5
Theroiz <164636377+Theroiz@users.noreply.github.com>
2024-06-21 20:38:17 -0300

    Add files via upload

---
+++ src/NewClas.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class NewClas {
+    public static void main(String[]args)
+    {
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Insira sua senha");
+        int senha = entrada.nextInt();
+        System.out.println("Sua senha sera: "+senha);
+        
+    }
+}
